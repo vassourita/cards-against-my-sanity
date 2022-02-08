8079260ebf667bd70b305e572144105435d913cf
Vinicius Vassão <vlviniciusguaruja7@gmail.com>
2022-02-08 14:27:01 -0300

    Setup configuration files
    Co-authored-by: Kauê Gatto <kauegatto123@gmail.com>

--- server/src/CardsAgainstMySanity.Presentation/Program.cs
+++ server/src/CardsAgainstMySanity.Presentation/Program.cs
@@ -1,7 +1,10 @@
+using CardsAgainstMySanity.Infrastructure.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCardsAgainstMySanity(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
