5607ef4a73bf15c4e0274178bf7b4c70115137b2
kaue gatto <kauegatto123@gmail.com>
2022-02-18 21:32:14 -0300

    test: initial AccessServiceTest

--- server/test/CardsAgainstMySanity.Domain.Test/Auth/Services/AccesServiceTest.cs
+++ server/test/CardsAgainstMySanity.Domain.Test/Auth/Services/AccesServiceTest.cs
@@ -1,37 +1,44 @@
-
-using CardsAgainstMySanity.Domain.Auth.Services;
+using Moq;
 using Xunit;
-using Microsoft.Extensions.DependencyInjection;
-using CardsAgainstMySanity.Domain.Auth.Repositories;
-using Microsoft.Extensions.Logging;
+using FluentAssertions;
+using CardsAgainstMySanity.Domain.Auth.Services;
 using CardsAgainstMySanity.Domain.Auth.Tokens;
 using CardsAgainstMySanity.Domain.Providers;
-using FluentAssertions;
 using CardsAgainstMySanity.Test;
+using CardsAgainstMySanity.Domain.Auth.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using CardsAgainstMySanity.SharedKernel;
+using System.Security.Claims;
 
 namespace CardsAgainstMySanity.Domain.Test.Auth.Services
 {
     public class AccessServiceTest : TestBase
-    {   
+    {
         private readonly ILogger<AccessService> _logger;
         public AccessServiceTest()
         {
             var _loggerFactory = new LoggerFactory();
             _logger = LoggerFactoryExtensions.CreateLogger<AccessService>(_loggerFactory);
         }
-        
+
         #region factories
-        private MakeFakeTokenService () =>
-            new FakeTokenService()
+        private Mock<TokenService> MakeMockTokenService()
+        {
+            var mock = new Mock<TokenService>();
+            mock.Setup(x => x.IsAccessTokenValid("valid-token"))
+                .Returns(Result<ClaimsPrincipal, TokenService.ValidationError>.Ok(new ClaimsPrincipal()));
+            return mock;
+        }
         private AccessService MakeSut() => new AccessService(
             ServiceProvider.GetRequiredService<IRefreshTokenRepository>(),
-            MakeFakeTokenService(),
+            MakeMockTokenService().Object,
             ServiceProvider.GetRequiredService<IGuestRepository>(),
             _logger
             );
 
         private IDateTimeProvider MakeDateTimeProvider() => ServiceProvider.GetRequiredService<IDateTimeProvider>();
-        
+
         private RefreshToken MakeRefreshToken() => new RefreshToken(
             System.Guid.NewGuid(),
             MakeDateTimeProvider().UtcNow.AddMinutes(2),
@@ -41,16 +48,17 @@ namespace CardsAgainstMySanity.Domain.Test.Auth.Services
         #endregion
         #region tests
         [Fact]
-        public async void ValidateUserTokens_ValidTokens_ShouldSucceed(){
+        public async void ValidateUserTokens_ValidTokens_ShouldSucceed()
+        {
             var refreshToken = MakeRefreshToken();
             // Arrange
 
             var sut = MakeSut();
-            
+
             // Act
 
             var result = await sut.ValidateUserTokens("valid-token", refreshToken.Token);
-            
+
             // Assert
             result.Succeeded.Should().BeTrue();
             result.Data.Should().NotBeNull();
